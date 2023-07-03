febe99e2f4ba9374759c8d9b660e87224c8bc118
unknown <eliezermasiala2000@gmail.com>
2023-07-03 14:24:02 +0200

    version one

--- chengedflag.js
+++ chengedflag.js
@@ -1,28 +1,29 @@
+const body= document.querySelector('body')
 
-function chengedFlag(){
+async function chengedFlag(){
 
-    const body= document.querySelector('body')
     const buttons=document.querySelectorAll('a').forEach(buttons=>{
         
-
         buttons.addEventListener('click',(e)=>{
-
-            const result= e.currentTarget.firstChild.nodeValue;
+            const value = e.target.innerHTML;
             
-            if(result.length <= 3){
+            if (value === "RDC"){
 
                 body.style.backgroundImage= 'url("./flag/Flag_of_the_Democratic_Republic_of_the_Congo.svg")';
                 body.style.backgroundSize= 'cover'
-            } 
-            else if(result.length <= 6){
+
+            } else if (value === 'IVOIR'){
+
+                body.style.backgroundImage = 'url("./flag/cote ivoir.png")'
+                body.style.backgroundSize= 'cover'
+
+            } else if (value === 'BRAZZA'){
+
                 body.style.backgroundImage= ' url("./flag/Flag_of_the_Republic_of_the_Congo.svg.png")';
                 body.style.backgroundSize= 'cover';
-            } 
-            /* else if(result.length <= 5) {
-                body.style.background= 'red'
-            } */
 
-        })
+            }
+        }) 
     })
 }
 
